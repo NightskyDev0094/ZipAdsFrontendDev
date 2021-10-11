cc213414cc5110de644f1409a01f88c4b9f66365
Tony Wheeler <42426319+twheelertech@users.noreply.github.com>
2021-10-11 13:13:33 -0500

    Changed dev server on frontend.

--- src/environmentVariables.js
+++ src/environmentVariables.js
@@ -2,16 +2,10 @@
  * Temporary environmental file until we set up
  * process capabilitys
  * */
-const FACEBOOK_KEY = '230330218311850';
-//  Test google creds
-const GOOGLE_DEVELOPER_TOKEN = '5DxAhDbX1x54iZD7uO5nTQ';
-const GOOGLE_KEY = '1062228155409-ggkaciv0u6dc23l2235o3jfidnv65rg1.apps.googleusercontent.com';
-const GOOGLE_OAUTH2_KEY =
-  '1062228155409-ggkaciv0u6dc23l2235o3jfidnv65rg1.apps.googleusercontent.com';
-//  Production google creds
-// const GOOGLE_DEVELOPER_TOKEN = 'nNSk0jhRWDeQ8mlDUg8deg';
-// const GOOGLE_KEY = '708567065448-augb7pvde2un1a07t6vs6d4r36mmq32m.apps.googleusercontent.com';
-// const GOOGLE_OAUTH2_KEY =
-//   '708567065448-augb7pvde2un1a07t6vs6d4r36mmq32m.apps.googleusercontent.com';
-const SERVER_URL = 'http://localhost:8000';
+ const FACEBOOK_KEY = process.env.FACEBOOK_KEY,
+ GOOGLE_DEVELOPER_TOKEN = process.env.GOOGLE_DEVELOPER_TOKEN,
+ GOOGLE_KEY = process.env.GOOGLE_KEY,
+ GOOGLE_OAUTH2_KEY = process.env.GOOGLE_OAUTH2_KEY,
+ SERVER_URL = 'https://web-ads-dev-server.herokuapp.com';
+
 export { FACEBOOK_KEY, GOOGLE_DEVELOPER_TOKEN, GOOGLE_KEY, GOOGLE_OAUTH2_KEY, SERVER_URL };
